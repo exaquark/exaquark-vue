c062e00ee87d8bad0275a53d09851ea69ae01cae
Paul Copplestone <pcopplestone@gmail.com>
2018-02-04 20:58:11 +0800

    Task: remove boilerplate code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,8 @@
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
