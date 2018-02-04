552c00de67bd82306c9dc04282e4079a528a20cb
Paul Copplestone <pcopplestone@gmail.com>
2018-02-04 20:57:11 +0800

    Task: refactor boilerplate code

---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Nav">
+
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+      </a>
+
+      <!-- <button class="button navbar-burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button> -->
+    </div>
+  </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+<style scoped lang="scss">
+</style>
